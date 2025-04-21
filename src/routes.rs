5e7eba83ffe26205a1a10e1f4daa9625bc812b70
Sulaman0 <symikhan70@gmail.com>
2025-04-21 11:42:51 +0500

    C13

--- src/routes.rs
+++ src/routes.rs
@@ -2,8 +2,13 @@ use actix_web::{get, HttpResponse, Responder};
 use crate::service::{{test1}};
 
 
+// main function!
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body(test1())
 }
-// main function!
+
+#[get()]
+// async fn profile() -> impl Responder {
+//     HttpResponse::Ok().body(test1())
+// }
