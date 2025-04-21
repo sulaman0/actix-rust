e0410644179b998318fa6e8d2b280d85a1a8437c
Sulaman0 <symikhan70@gmail.com>
2025-04-21 11:49:16 +0500

    C32

--- src/routes.rs
+++ src/routes.rs
@@ -9,7 +9,7 @@ async fn hello() -> impl Responder {
 }
 
 #[get("/profile")]
-async fn profi
+async fn profil
 // async fn profile() -> impl Responder {
 //     HttpResponse::Ok().body(test1())
 // }
