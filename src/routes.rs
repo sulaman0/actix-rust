c9754dcfa95040dea63b03ccff1c76005f1e72fe
Sulaman0 <symikhan70@gmail.com>
2025-04-21 11:43:45 +0500

    C15

--- src/routes.rs
+++ src/routes.rs
@@ -8,7 +8,7 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body(test1())
 }
 
-#[get("/p")]
+#[get("/pr")]
 // async fn profile() -> impl Responder {
 //     HttpResponse::Ok().body(test1())
 // }
